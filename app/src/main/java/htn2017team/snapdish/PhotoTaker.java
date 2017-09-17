deab0325946def29d5bd50f5891eb60d6da82da4
Sharon Wang <sharon-wang-cpsc@outlook.com>
2017-09-16 21:26:27 -0400

    Camera integrated, need to fix skew

--- app/src/main/java/htn2017team/snapdish/PhotoTaker.java
+++ app/src/main/java/htn2017team/snapdish/PhotoTaker.java
@@ -1,25 +1,34 @@
 package htn2017team.snapdish;
 
-import android.annotation.SuppressLint;
-//import android.graphics.Camera;
-import android.support.v7.app.ActionBar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
 import android.hardware.Camera;
+import android.util.Log;
+import android.widget.FrameLayout;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class PhotoTaker extends AppCompatActivity {
+    private Camera mCamera;
+    private CameraPreview cameraPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phototaker);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        cameraPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(cameraPreview);
     }
 
     /** A safe way to get an instance of the Camera object. */
@@ -31,7 +40,22 @@ public class PhotoTaker extends AppCompatActivity {
         }
         catch (Exception e){
             // Camera is not available (in use or does not exist)
+            Log.d(TAG, "Error getting camera instance: " + e.getMessage());
+            e.printStackTrace();
         }
         return c; // returns null if camera is unavailable
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();              // release the camera immediately on pause event
+    }
+
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
     }
 }
