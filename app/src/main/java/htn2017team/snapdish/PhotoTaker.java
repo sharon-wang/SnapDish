4d276be1a0457e3a9bc547f51b98a08ff3e89d12
Elliot Evans <elliot.evans@rallyteam.com>
2017-09-17 01:18:43 -0400

    add gallery and watson

--- app/src/main/java/htn2017team/snapdish/PhotoTaker.java
+++ app/src/main/java/htn2017team/snapdish/PhotoTaker.java
@@ -2,13 +2,38 @@ package htn2017team.snapdish;
 
 import android.annotation.SuppressLint;
 //import android.graphics.Camera;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.hardware.Camera;
+import android.widget.Button;
+
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyImagesOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.io.OutputStream;
+import java.io.File;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -16,10 +41,76 @@ import android.hardware.Camera;
  */
 public class PhotoTaker extends AppCompatActivity {
 
+    public static final int PICK_IMAGE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phototaker);
+
+        //pick file
+        Button button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //pick file
+                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                getIntent.setType("image/*");
+
+                Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                pickIntent.setType("image/*");
+
+                Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+                startActivityForResult(chooserIntent, PICK_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, final Intent data)
+    {
+        if (requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK) {
+            if (data == null) {
+                //Display an error
+                return;
+            }
+            try {
+                Thread thread = new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try  {
+                            InputStream inputStream = getApplicationContext().getContentResolver().openInputStream(data.getData());
+                            File out = File.createTempFile("afileofanimage", "png");
+                            copyInputStreamToFile(inputStream, out);
+                            VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_20);
+                            service.setApiKey("<api-key>");
+
+                            System.out.println("Classify an image");
+                            ClassifyImagesOptions options = new ClassifyImagesOptions.Builder()
+                                    .images(out)
+                                    .build();
+                            VisualClassification result = service.classify(options).execute();
+                            Log.e("l", result.toString());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                thread.start();
+
+            }
+            catch (Exception e) {
+                Log.e("hi",e.toString());
+            }
+
+
+
+            //Now you can do whatever you want with your inpustream, save it as file, upload to a server, decode a bitmap...
+        }
     }
 
     /** A safe way to get an instance of the Camera object. */
@@ -33,5 +124,20 @@ public class PhotoTaker extends AppCompatActivity {
             // Camera is not available (in use or does not exist)
         }
         return c; // returns null if camera is unavailable
+    }
+
+    private void copyInputStreamToFile( InputStream in, File file ) {
+        try {
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while((len=in.read(buf))>0){
+                out.write(buf,0,len);
+            }
+            out.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
