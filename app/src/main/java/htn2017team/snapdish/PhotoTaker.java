caba311f39ea22dbdd76b4ee6380a7c15a14f9e9
Sharon Wang <sharon-wang-cpsc@outlook.com>
2017-09-16 23:11:02 -0400

    Image persists on screen when photo snapped

--- app/src/main/java/htn2017team/snapdish/PhotoTaker.java
+++ app/src/main/java/htn2017team/snapdish/PhotoTaker.java
@@ -1,13 +1,25 @@
 package htn2017team.snapdish;
 
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.Camera;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static android.content.ContentValues.TAG;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -16,6 +28,7 @@ import static android.content.ContentValues.TAG;
 public class PhotoTaker extends AppCompatActivity {
     private Camera mCamera;
     private CameraPreview cameraPreview;
+    private Button shutterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +39,18 @@ public class PhotoTaker extends AppCompatActivity {
         mCamera = getCameraInstance();
 
         // Create our Preview view and set it as the content of our activity.
+        mCamera.setDisplayOrientation(90);
         cameraPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(cameraPreview);
+
+        shutterButton = (Button) findViewById(R.id.button_capture);
+        shutterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCamera.takePicture(null, null, mPicture);
+            }
+        });
     }
 
     /** A safe way to get an instance of the Camera object. */
@@ -46,6 +68,29 @@ public class PhotoTaker extends AppCompatActivity {
         return c; // returns null if camera is unavailable
     }
 
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions.");
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+
     @Override
     protected void onPause() {
         super.onPause();
@@ -57,5 +102,39 @@ public class PhotoTaker extends AppCompatActivity {
             mCamera.release();        // release the camera for other applications
             mCamera = null;
         }
+    }
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
     }
 }
